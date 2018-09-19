affcca65af9e8bd99421aa0ad645d33196c7aa29
Zachery Gentry <zacherygentry@gmail.com>
2018-09-19 16:53:27 -0500

    restructure files

---
+++ lab1/README.md
@@ -0,0 +1,2 @@
+# cse-3302-labs
+My code for CSE-3302 Programming Languages class  
---
+++ lab1/zcg4385.js
@@ -0,0 +1,8 @@
+// Zachery Gentry
+// 1001144385
+// 09/19/2018
+
+
+let inputtable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+print("ll");
