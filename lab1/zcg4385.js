2f0ca65a179ce814d069fd6b2eb241e857356a0b
Zachery Gentry <zacherygentry@gmail.com>
2018-09-19 21:58:45 -0500

    finished closure example

--- lab1/zcg4385.js
+++ lab1/zcg4385.js
@@ -7,31 +7,44 @@
 const inputtable = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const fiveTable = inputtable.map(value => value * 5);
-console.log(fiveTable);
+console.log('fiveTable: ' + fiveTable);
 
 const thirteenTable = inputtable.map(value => value * 13);
-console.log(thirteenTable);
+console.log('\nthirteenTable: ' + thirteenTable);
 
 const squaresTable = inputtable.map(value => value * value);
-console.log(squaresTable);
+console.log('\nsquaresTable: ' + squaresTable);
 
 const inputThrough100 = new Array(100).fill(0).map((value, index) => index + 1)
-console.log(inputThrough100);
 
 const oddMultiplesOf5 = inputThrough100.filter(value => value % 5 == 0 && value % 2 != 0);
-console.log(oddMultiplesOf5);
+console.log('\noddMultiplesOf5: ' + oddMultiplesOf5);
 
 const evenMultiplesOf7Sum = inputThrough100
     .filter(value => value % 7 == 0 && value % 2 == 0)
     .reduce((sum, curr) => sum + curr, 0);
 
-console.log(evenMultiplesOf7Sum);
+console.log('\nevenMultiplesOf7Sum: ' + evenMultiplesOf7Sum);
 
 const cylinder_volume = radius => height => 3.14 * radius * radius * height;
 
-console.log(cylinder_volume(5)(10));
+console.log('\ncylinder_volume: ' + cylinder_volume(5)(10));
 
+/*
 const makeTag = (beginTag, endTag) => textcontent => beginTag + textcontent + endTag;
-const makeTable = textcontent => makeTag('<table>','</table>'(textcontent));
-const makeRow = textcontent => makeTag('<tr>','</tr>'(textcontent));
-console.log(makeTag('(',')')('Hello'));
+const makeTable = textcontent => makeTag('<table>\n','\n</table>')(textcontent);
+const makeRow = textcontent => makeTag('<tr>\n','\n</tr>')(textcontent);
+const makeCell = textcontent => makeTag('<td>\n', '\n</td>')(textcontent);
+*/
+
+const makeTable = (tableBeg, tableEnd) => {
+    return  makeRow = (rowBeg, rowEnd) => {
+        return makeCell = (cellBeg, cellEnd, textcontent) => {
+            return tableBeg + rowBeg + cellBeg + textcontent + cellEnd + rowEnd + tableEnd;
+        }
+    }
+}
+
+const row = makeTable('<table>\n', '\n</table>');
+const cell = row('<tr>\n', '\n</tr>');
+console.log('\nHTML Table\n' + cell('<td>\n','\n</td>','This is my cell in a row in a table!'));
