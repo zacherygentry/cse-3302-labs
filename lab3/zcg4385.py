c21e2662d51bf8232dd1b0d37a79b00fc5420781
Zachery Gentry <zacherygentry@gmail.com>
2018-11-12 21:16:01 -0600

    added more operators

--- lab3/zcg4385.py
+++ lab3/zcg4385.py
@@ -2,6 +2,7 @@
 # 1001144385
 # 11/12/18
 # macOS
+# Operators accepted: %, ^, +, -, *, /
 
 
 # This functions takes in an equation in RPN format and returns the final solution to it
@@ -27,13 +28,34 @@ def CalculateRPN(line):
             secondNum = numbers.pop()
             number = secondNum / firstNum
             numbers.append(number)
+        elif character == "%":
+            firstNum = numbers.pop()
+            secondNum = numbers.pop()
+            number = secondNum % firstNum
+            numbers.append(number)
+        elif character == "^":
+            firstNum = numbers.pop()
+            secondNum = numbers.pop()
+            number = pow(secondNum, firstNum)
+            numbers.append(number)
         # If the character is just a number, add it to the stack
         elif character != "\n":
             numbers.append(int(character))
     return numbers.pop()  # The last item of the stack should be the final solution
 
 
-file = open("input.txt", "r")
+# def ConvertToRPN(line):
+#     numbers = []
+#     operators = []
+
+#     RPNLine = ""
+
+#     for character in line.split():
+#         if character == "(":
+#             print("In parenth")
+
+
+file = open("input_RPN.txt", "r")
 
 for line in file:
     print(CalculateRPN(line))
